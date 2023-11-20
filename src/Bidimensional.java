c53776c983ab7bc6e7fee3978b60cdc5f126718e
João Pedro <jpronzanisantos@gmail.com>
2023-11-19 21:00:06 -0300

    Teste

--- src/Bidimensional.java
+++ src/Bidimensional.java
@@ -1,11 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 import java.util.Objects;
 import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 
 public class Bidimensional {
 
@@ -15,20 +10,18 @@ public class Bidimensional {
         if (s) {
             System.out.print("Digite a palavra: ");
             String palavra = scanner.nextLine();
-            salvarMatrizEmArquivo("matriz.txt", codificarParidadeASCII(palavra));
+            salvarMatrizEmArquivo("matriz.txt", encode(palavra));
         } else {
-            boolean integro = verificarIntegridade(lerMatrizDoArquivo("matriz.txt"));
+            boolean integro = validateParity(lerMatrizDoArquivo("matriz.txt"));
             System.out.println("Integro? " + (integro ? "sim" : "não"));
         }
     }
 
     private static void salvarMatrizEmArquivo(String nomeArquivo, int[][] matriz) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
-            // Escrever o número de linhas e colunas da matriz
             writer.write(matriz.length + " " + matriz[0].length);
             writer.newLine();
 
-            // Escrever os valores da matriz
             for (int i = 0; i < matriz.length; i++) {
                 for (int j = 0; j < matriz[i].length; j++) {
                     writer.write(Integer.toString(matriz[i][j]) + " ");
@@ -40,134 +33,98 @@ public class Bidimensional {
         }
     }
 
-    private static int[][] lerMatrizDoArquivo(String nomeArquivo) {
+    private static int[][] lerMatrizDoArquivo(String path) {
         int[][] matriz = null;
-
-        try  {
-            BufferedReader br = new BufferedReader(new FileReader(nomeArquivo));
-            String colunaLinha = br.readLine();
-            int linhas = Integer.parseInt(String.valueOf(colunaLinha.split(" ")[0]));
-            int colunas = Integer.parseInt(String.valueOf(colunaLinha.split(" ")[1]));
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String[] dimensoes = br.readLine().split(" ");
+            int linhas = Integer.parseInt(dimensoes[0]);
+            int colunas = Integer.parseInt(dimensoes[1]);
 
             matriz = new int[linhas][colunas];
+
             for (int i = 0; i < linhas; i++) {
-                String linha = br.readLine();
+                String[] linha = br.readLine().split(" ");
                 for (int j = 0; j < colunas; j++) {
-                    matriz[i][j] = Character.getNumericValue(linha.charAt(j));
+                    matriz[i][j] = Integer.parseInt(linha[j]);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return matriz;
     }
 
-    private static boolean verificarIntegridade(int[][] matrizBits) {
-        // Verifica paridade por linha
-        for (int i = 0; i < matrizBits.length; i++) {
-            int paridadeLinha = calcularParidade(Arrays.copyOfRange(matrizBits[i], 0, matrizBits[i].length - 1));
-            if (paridadeLinha != matrizBits[i][matrizBits[i].length - 1]) {
+    public static boolean validateParity(int[][] matrizComParidade) {
+        int linhas = matrizComParidade.length;
+        int colunas = matrizComParidade[0].length;
+
+        for (int i = 0; i < linhas - 1; i++) {
+            int paridadeLinha = 0;
+            for (int j = 0; j < colunas - 1; j++) {
+                paridadeLinha ^= matrizComParidade[i][j];
+            }
+            if (paridadeLinha != matrizComParidade[i][colunas - 1]) {
+                System.out.println("Erro de paridade na linha " + i);
                 return false;
             }
         }
 
-        // Verifica paridade por coluna
-        for (int j = 0; j < matrizBits[0].length; j++) {
-            int[] coluna = new int[matrizBits.length - 1];
-            for (int i = 0; i < matrizBits.length - 1; i++) {
-                coluna[i] = matrizBits[i][j];
+        for (int j = 0; j < colunas - 1; j++) {
+            int paridadeColuna = 0;
+            for (int i = 0; i < linhas - 1; i++) {
+                paridadeColuna ^= matrizComParidade[i][j];
             }
-            int paridadeColuna = calcularParidade(coluna);
-            if (paridadeColuna != matrizBits[matrizBits.length - 1][j]) {
+            if (paridadeColuna != matrizComParidade[linhas - 1][j]) {
+                System.out.println("Erro de paridade na coluna " + j);
                 return false;
             }
         }
 
+        System.out.println("Paridade verificada com sucesso!");
         return true;
     }
 
-    private static int[][] codificarParidadeASCII(String palavra) {
-        int[][] matrizBits = converterParaMatrizBits(palavra);
-        adicionarParidadeBidimensional(matrizBits);
-        System.out.println("\nMatriz Codificada:");
-        imprimirMatriz(matrizBits);
-        return matrizBits;
+    public static int[][] encode(String palavra) {
+        int[][] matrizOriginal = convertToBinaryMatrix(palavra);
+        int linhas = matrizOriginal.length;
+        int colunas = matrizOriginal[0].length;
 
+        int[][] matrizComParidadeLinha = new int[linhas][colunas + 1];
+        for (int i = 0; i < linhas; i++) {
+            int paridadeLinha = 0;
+            for (int j = 0; j < colunas; j++) {
+                matrizComParidadeLinha[i][j] = matrizOriginal[i][j];
+                paridadeLinha ^= matrizOriginal[i][j];
+            }
+            matrizComParidadeLinha[i][colunas] = paridadeLinha;
+        }
+
+        int[][] matrizComParidade = new int[linhas + 1][colunas + 1];
+        for (int j = 0; j < colunas; j++) {
+            int paridadeColuna = 0;
+            for (int i = 0; i < linhas; i++) {
+                paridadeColuna ^= matrizOriginal[i][j];
+            }
+            matrizComParidade[linhas][j] = paridadeColuna;
+        }
+
+        for (int i = 0; i < linhas; i++) {
+            System.arraycopy(matrizComParidadeLinha[i], 0, matrizComParidade[i], 0, colunas + 1);
+        }
+
+        return matrizComParidade;
     }
 
-    private static int[][] converterParaMatrizBits(String palavra) {
-        int[][] matrizBits = new int[palavra.length() + 1][8];
-
-        // Preenche a matriz com os bits da palavra
+    public static int[][] convertToBinaryMatrix(String palavra) {
+        int[][] matriz = new int[palavra.length()][8];
         for (int i = 0; i < palavra.length(); i++) {
-            char caracter = palavra.charAt(i);
+            char c = palavra.charAt(i);
             for (int j = 0; j < 8; j++) {
-                matrizBits[i][j] = (caracter >> (7 - j)) & 1;
+                matriz[i][j] = (c >> (7 - j)) & 1;
             }
         }
-
-        return matrizBits;
-    }
-
-    private static void adicionarParidadeBidimensional(int[][] matrizBits) {
-        char[] caracteres = palavra.toCharArray();
-
-        // Calcular o número de bits de paridade necessário
-        int numBitsParidade = 0;
-        while (Math.pow(2, numBitsParidade) < caracteres.length + numBitsParidade + 1) {
-            numBitsParidade++;
-        }
-
-        // Adicionar bits de paridade à lista
-        StringBuilder palavraComParidade = new StringBuilder();
-        int indiceParidade = 0;
-        for (int i = 0; i < caracteres.length + numBitsParidade; i++) {
-            if ((i + 1) == Math.pow(2, indiceParidade)) {
-                palavraComParidade.append('P');
-                indiceParidade++;
-            } else {
-                palavraComParidade.append(caracteres[i - indiceParidade]);
-            }
-        }
-
-        // Criar uma matriz bidimensional
-        int[][] matriz = new int[caracteres.length + numBitsParidade][numBitsParidade];
-
-        // Preencher a matriz com os valores da palavra e bits de paridade
-        for (int i = 0; i < matriz.length; i++) {
-            String binario = new StringBuilder(Integer.toBinaryString(i + 1))
-                    .reverse().toString();
-            for (int j = 0; j < numBitsParidade; j++) {
-                matriz[i][j] = Integer.parseInt(String.valueOf(binario.charAt(j)));
-            }
-        }
-
         return matriz;
     }
 
-    private static int calcularParidade(int[] linha) {
-        int soma = 0;
-        for (int bit : linha) {
-            soma += bit;
-        }
-        return soma % 2;
-    }
 
-    private static int calcularParidadePorColuna(int[][] matrizBits, int coluna) {
-        int soma = 0;
-        for (int i = 0; i < matrizBits.length - 1; i++) {
-            soma += matrizBits[i][coluna];
-        }
-        return soma % 2;
-    }
-
-    private static void imprimirMatriz(int[][] matrizBits) {
-        for (int[] matrizBit : matrizBits) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(matrizBit[j] + " ");
-            }
-            System.out.println();
-        }
-    }
 }
